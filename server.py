1c1ce9743b0a4d288d6ccace85e1cbc57237133e
Pete Wildsmith <pete@dabapps.com>
2016-04-14 20:10:40 +0100

    streamed response bodies, with all the datas

--- server.py
+++ server.py
@@ -2,10 +2,11 @@ import datetime
 import falcon
 import json
 
+from json import JSONEncoder
+
 import os
 import psycopg2cffi.pool
 import dj_database_url
-from psycopg2cffi._impl.connection import Connection
 from wsgi_basic_auth import BasicAuth
 from psycopg2cffi.extras import Json
 
@@ -17,6 +18,39 @@ pool = psycopg2cffi.pool.ThreadedConnectionPool(1, 10,
                                                 port=5432,
                                                 password=database_config['PASSWORD'],
                                                 database=database_config['NAME'])
+
+
+class DateAndDateTimeSupportingJSONEncoder(JSONEncoder):
+    def default(self, o):
+        if isinstance(o, (datetime.date, datetime.datetime)):
+            return o.isoformat()
+
+        return super(DateAndDateTimeSupportingJSONEncoder, self).default(o)
+
+def json_stream(obj, skipkeys=False, ensure_ascii=True, check_circular=True,
+         allow_nan=True, cls=None, indent=None, separators=None,
+         encoding='utf-8', default=None, sort_keys=False, **kw):
+    """Serialize ``obj`` and yield chunks.
+
+    """
+    # cached encoder
+    if (not skipkeys and ensure_ascii and
+            check_circular and allow_nan and
+                cls is None and indent is None and separators is None and
+                encoding == 'utf-8' and default is None and not sort_keys and not kw):
+        iterable = DateAndDateTimeSupportingJSONEncoder().iterencode(obj)
+    else:
+        if cls is None:
+            cls = DateAndDateTimeSupportingJSONEncoder
+        iterable = cls(skipkeys=skipkeys, ensure_ascii=ensure_ascii,
+                       check_circular=check_circular, allow_nan=allow_nan, indent=indent,
+                       separators=separators, encoding=encoding,
+                       default=default, sort_keys=sort_keys, **kw).iterencode(obj)
+    # could accelerate with writelines in some versions of Python, at
+    # a debuggability cost
+    for chunk in iterable:
+        yield chunk
+
 
 class JSONTranslator(object):
 
@@ -42,8 +76,7 @@ class JSONTranslator(object):
     def process_response(self, req, resp, resource):
         if 'result' not in req.context:
             return
-
-        resp.data = json.dumps(req.context['result'])
+        resp.stream = json_stream(req.context['result'])
 
 class PostgresConnectionPool(object):
 
@@ -93,23 +126,57 @@ class MismatchDataResource:
             cur.execute("INSERT INTO failures (name, data) VALUES (%s, %s)", [name, Json(doc)])
 
 
+class StreamedDict(dict):
+    def __init__(self, source, f=lambda x: x):
+        self.source = source
+        self.f = f
+
+    def iteritems(self):
+        for x in self.source:
+            yield self.f(x)
+
+    def __len__(self):
+        return 1
+
+
+class StreamedList(list):
+    def __init__(self, source, f=lambda x: x):
+        self.source = source
+        self.f = f
+
+    def __iter__(self):
+        for x in self.source:
+            yield self.f(x)
+
+    def __len__(self):
+        return 1
+
+
+
 class DataDownloadResource:
     """
     Big JSON document, with all your datas.
     """
     def on_get(self, req, resp):
         with req.context['connection'] as conn:
-            cur = conn.cursor()
-            cur.execute("SELECT * from matches")
-            match_data = {
-                record[0].isoformat(): {
+            match_cur = conn.cursor()
+            match_cur.execute("SELECT * from matches")
+            print "done first select"
+
+            def date_match_record(record):
+                return record[0].isoformat(), {
                     'success': record[1],
                     'failure': record[2],
                 }
-                for record in cur
-            }
+
+            failure_cur = conn.cursor()
+            failure_cur.execute("select * from failures")
+            print "done second select"
             resp.content_type = 'application/json'
-            req.context['result'] = match_data
+            req.context['result'] = {
+                'stats': StreamedDict(match_cur, date_match_record),
+                'failures': list(failure_cur)
+            }
 
 
 app = falcon.API(middleware=[
